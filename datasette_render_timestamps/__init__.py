5e05d653d38ba985c47cf00a0030e454359a1a2c
chris48s <chris.shaw480@gmail.com>
2019-10-13 19:36:14 +0100

    Allow user to specify date/time format via config

--- datasette_render_timestamps/__init__.py
+++ datasette_render_timestamps/__init__.py
@@ -11,14 +11,21 @@ max_timestamp = int(
         (datetime.datetime.utcnow() + datetime.timedelta(days=5 * 365))
     )
 )
+default_format = "%B %d, %Y - %H:%M:%S UTC"
 
 
 @hookimpl()
-def render_cell(value):
+def render_cell(value, column, table, database, datasette):
+    config = (
+        datasette.plugin_config("datasette-render-timestamps", database=database, table=table)
+        or {}
+    )
+    if "format" not in config:
+        config["format"] = default_format
     if not isinstance(value, int):
         return None
     # Is it within the range we care about?
     if not (min_timestamp < value < max_timestamp):
         return None
     dt = datetime.datetime.utcfromtimestamp(value)
-    return dt.strftime("%B %d, %Y - %H:%M:%S UTC")
+    return dt.strftime(config["format"])
