4e8685134928f0c83ce881d23734de2457180071
Simon Willison <swillison@gmail.com>
2020-03-21 14:15:30 -0700

    Ensure tests don't fail in five years time
    I had hard-coded in a timestamp which means these tests would start
    to fail when that timestamp was no longer within five years of
    current clock time.

--- tests/test_render_timestamps.py
+++ tests/test_render_timestamps.py
@@ -1,27 +1,45 @@
 from unittest import mock
 from datasette.app import Datasette
 from datasette_render_timestamps import render_cell
+import datetime
+import pytest
 
 
-def test_render_timestamps():
-    assert "October 10, 2019 - 07:18:29 UTC" == render_cell(
-        1570691909, None, None, None, Datasette([])
-    )
+@pytest.fixture
+def timestamp_within_five_years():
+    current_year = datetime.date.today().year
+    dt = datetime.datetime(current_year, 10, 10, 7, 18, 29)
+    return int(dt.replace(tzinfo=datetime.timezone.utc).timestamp())
+
+
+def test_render_timestamps(timestamp_within_five_years):
+    assert "October 10, {} - 07:18:29 UTC".format(
+        datetime.date.today().year
+    ) == render_cell(timestamp_within_five_years, None, None, None, Datasette([]))
 
 
 def test_do_not_render_low_numbers():
     assert None == render_cell(1, None, None, None, Datasette([]))
 
 
-def test_do_not_render_high_numbers():
-    assert None == render_cell(15706919090, None, None, None, Datasette([]))
+def test_do_not_render_high_numbers(timestamp_within_five_years):
+    assert None == render_cell(
+        timestamp_within_five_years * 1000, None, None, None, Datasette([])
+    )
 
 
-@mock.patch(
-    "datasette.app.Datasette.plugin_config",
-    lambda *args, **kwargs: {"format": "%Y-%m-%d-%H:%M:%S"},
-)
-def test_custom_format():
-    assert "2019-10-10-07:18:29" == render_cell(
-        1570691909, None, None, None, Datasette([])
+def test_custom_format(timestamp_within_five_years):
+    assert "{}-10-10 07:18:29".format(datetime.date.today().year) == render_cell(
+        timestamp_within_five_years,
+        column="timestamp",
+        table="mytable",
+        database="mydatabase",
+        datasette=Datasette(
+            [],
+            metadata={
+                "plugins": {
+                    "datasette-render-timestamps": {"format": "%Y-%m-%d %H:%M:%S"}
+                }
+            },
+        ),
     )
